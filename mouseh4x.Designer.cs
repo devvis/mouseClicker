45f5a0c14c5afcf60b1f4c916843ba63294906cd
Gustav Eklundh <gustav@xcoders.info>
2013-08-26 01:54:27 +0200

    Initial commit

---
+++ mouseh4x.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace mouseClicker
+{
+    partial class mainWnd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mouzh4ckz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mouzh4ckz
+            // 
+            this.mouzh4ckz.AutoSize = true;
+            this.mouzh4ckz.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mouzh4ckz.Location = new System.Drawing.Point(16, 9);
+            this.mouzh4ckz.Name = "mouzh4ckz";
+            this.mouzh4ckz.Size = new System.Drawing.Size(105, 14);
+            this.mouzh4ckz.TabIndex = 0;
+            this.mouzh4ckz.Text = "I h4ck ur mouz";
+            // 
+            // mainWnd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(139, 31);
+            this.Controls.Add(this.mouzh4ckz);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "mainWnd";
+            this.Opacity = 0.8D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MOUSE H4XX0RZZzzz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mouzh4ckz;
+    }
+}
+
---
+++ mouseh4x.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace mouseClicker
+{
+    public partial class mainWnd : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+        protected override void WndProc(ref Message m)
+        {
+
+            if (m.Msg == 0x0312)
+            {
+                int id = m.WParam.ToInt32();
+                if (id == 0)
+                {
+                    CLICKALLTHEBUTTONS();
+                }
+                else if (id == 1)
+                {
+                    Environment.Exit(1337);
+                }
+                else
+                {
+                    MessageBox.Show("wut");
+                }
+
+            }
+
+            base.WndProc(ref m);
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+            
+        }
+
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public static Point GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+
+            return lpPoint;
+        }
+
+        public static void leftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+        public static void rightMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_RIGHTUP, xpos, ypos, 0, 0);
+        }
+
+        public static void CLICKALLTHEBUTTONS()
+        {
+
+            Point yolo = GetCursorPosition();
+            // Clicks the current position for 1000 times, mkay.
+            for (int i = 0; i < 1000; i++)
+            {
+                leftMouseClick(yolo.X, yolo.Y);
+                System.Threading.Thread.Sleep(10);
+            }
+            MessageBox.Show("I doen.");
+        }
+
+        public mainWnd()
+        {
+            InitializeComponent();
+            RegisterHotKey(this.Handle, 0, 0x0000, 0x42);
+            RegisterHotKey(this.Handle, 1, 0x0000, 0x43);
+        }
+    }
+}
