148007f4bc11e7e833ef7f250c0aecbdfa6811a6
Gustav Eklundh <gustav@xcoders.info>
2013-09-06 19:04:23 +0200

    Added an icon.

--- mouseh4x.Designer.cs
+++ mouseh4x.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWnd));
             this.mouzh4ckz = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -48,6 +49,7 @@
             this.ClientSize = new System.Drawing.Size(139, 31);
             this.Controls.Add(this.mouzh4ckz);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "mainWnd";
